b51359fc1164b253d377596afda7a0e504bbb6d9
Talafua Seumanu <144817556+talaseumanu@users.noreply.github.com>
2023-10-08 07:33:30 +1300

    Adding deliverable W8.1

---
+++ W8.1 Hello World Community/HW4.sql
@@ -0,0 +1,26 @@
+DROP VIEW IF EXISTS dbo.v_hello_world_load;
+Go
+
+CREATE VIEW dbo.v_hello_world_load
+AS
+
+/********************************************************************************************************
+-NAME: dbo.v_hello_world_load
+
+-PURPOSE: Create the Hello World - Load view
+
+-MODIFICATION LOG:
+Ver     Date            Author       Description
+---     ---------       --------     -----------
+1.0     7/10/2023       TSEUMANU     1. Built this script for EC IT143
+
+
+RUNTIME:
+1s
+
+NOTES:
+This script exists to help me learn step 4 of 8 in the Answer Focused Approach for T-SQL Data Manipulation
+************************************************************************************************************/
+
+SELECT 'Hello World' AS my_message,
+       GETDATE() AS current_date_time;
